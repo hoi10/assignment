bcb39b719c1b04574d7a247342d6d152683f3259
hoi10 <hoicute10@gmail.com>
2025-04-08 18:21:45 +0700

    Update char.h

--- include/char.h
+++ include/char.h
@@ -8,10 +8,10 @@ const int JUMP_HEIGHT = 150;
 const float GRAVITY = 0.8f;
 
 extern float obstacleSpeed; 
-const int OBSTACLE_WIDTH = 85;
-const int OBSTACLE_HEIGHT = 85;
+const int OBSTACLE_WIDTH = 90;
+const int OBSTACLE_HEIGHT = 90;
 const float SPEED_INCREMENT = 0.01f;
-const int OBSTACLE_Y = SCREEN_HEIGHT - OBSTACLE_HEIGHT - 100;
+const int OBSTACLE_Y = SCREEN_HEIGHT - OBSTACLE_HEIGHT - 80;
 
 const float BG_SPEED = 3.0f;
 #endif
